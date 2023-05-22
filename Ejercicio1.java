e69891e049a59c7b0309591ebdf886ccaca657ce
2123i3fabri <reyesufabricio@gmail.com>
2023-05-22 08:30:56 -0600

    practica

---
+++ Ejercicio1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Ejercicio1{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese un numerillo: ");
+        int num = input.nextInt();
+        if (num%2==0){
+            System.out.println("Es par ");
+        }else{
+            System.out.println("Impar");
+        }
+    }
+}
---
+++ EjercicioDos.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class EjercicioDos{
+    public static void main(String [] args ){
+        Scanner input = new Scanner(System.in);
+        Sytem.out.println("ingrese el valor en celsius");
+        int celsius = input.nextInt();
+        double fahrenheit =(9.0/5)*celsius + 32;
+        System.out.println("El resultado es:"+ fahrenheit)
+    }
+
+}
